284eaaa8019acdf42ad3737e49b99c7c084f727a
michaelfuchaka <fuchakamichael06@gmail.com>
2025-08-06 17:53:12 +0300

    Implement happy_new_year and fizzbuzz functions; refine square_integers and fizzbuzz_value logic

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+     return [num ** 2 for num in int_list]
+     pass
+
+def fizzbuzz_value(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):  # from 1 to 100 inclusive
+        print(fizzbuzz_value(num))
+
     pass
+
